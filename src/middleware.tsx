ba95b8366ff9f551ca0e52d0fe142a7bc2791ccc
Anwar Hossain <mahedianwar@gmail.com>
2023-09-08 15:45:57 +0600

    feat: added middlware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,18 +1,12 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import { authMiddleware } from '@clerk/nextjs';
 
-export default async function middleware(req: NextRequest) {
-  // const accessToken = req.cookies.get('accessToken')?.value;
-  // console.log('accessToken', accessToken);zz
-
-  const authUser = true;
-
-  if (!authUser)
-    return NextResponse.redirect(new URL('/unauthorized', req.url), req);
-
-  return NextResponse.next();
-}
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+export default authMiddleware({
+  publicRoutes: ['/api/uploadthing'],
+});
 
 export const config = {
-  matcher: ['/dashboard', '/dashboard/:path*'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
